eb625f15b7487d11f8ea6a33c5b1f68d261b7367
Liam Owen <l1amow3n5@yahoo.com>
2019-01-28 12:36:27 -0700

    made a header file named node

---
+++ CPPDataProject/Model/Node.hpp
@@ -0,0 +1,13 @@
+//
+//  Node.hpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Node_h
+#define Node_h
+
+
+#endif /* Node_h */
