e057935e013f80631ed3eb1ab84f9cbf337e2f71
Liam Owen <l1amow3n5@yahoo.com>
2019-01-28 12:46:29 -0700

    made a model header file

--- CPPDataProject/Model/Node.hpp
+++ CPPDataProject/Model/Node.hpp
@@ -9,5 +9,36 @@
 #ifndef Node_h
 #define Node_h
 
+#include <asser.h>
 
+template <class Type>
+class Node
+{
+private:
+    Type data;
+public:
+    Node(Type data);
+    Type getData();
+    void setData(Type data);
+};
+
+//Template definitions
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+template <class Type>
+void Node<Type> :: setData(Type data)
+{
+    this->data = data;
+}
 #endif /* Node_h */
