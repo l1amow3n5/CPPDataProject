4d9d2044df2371520d9eddef6018d7a9d66107a2
Liam Owen <l1amow3n5@yahoo.com>
2019-01-28 12:19:35 -0700

    made a new project and made some controller and runner files

---
+++ CPPDataProject/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ CPPDataProject/Controller/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <stdio.h>
