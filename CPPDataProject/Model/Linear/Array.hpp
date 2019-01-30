bb63d364c2d1bfbc781c5d7489f906ea447ebce3
Liam Owen <l1amow3n5@yahoo.com>
2019-01-30 13:28:51 -0700

    added the operators to the assignment section

--- CPPDataProject/Model/Linear/Array.hpp
+++ CPPDataProject/Model/Linear/Array.hpp
@@ -65,4 +65,44 @@ Array<Type> :: ~Array()
 {
     delete [] internalArray;
 }
+
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign != this)
+    {
+        if (size != toAssign.getSize())
+        {
+            delete [] internalArray;
+            size = toAssign.getSize();
+            internalArray = new Type [size];
+        }
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return *this;
+}
+
+template <class Type>
+Type & Array<Type> :: operator [] (int index)
+{
+    assert(index >= 0 && index < size);
+    return internalArray[index];
+}
+
+template <class Type>
+Type Array<Type> :: operator [] (int index) const
+{
+    assert(index >== && index < size);
+    return internalArray[index];
+}
+
+template <class Type>
+int Array<Type> :: getSize() const
+{
+    
+}
 #endif /* Array_h */
