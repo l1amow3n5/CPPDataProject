fc0d00a33b521cfca83bb1f931efa8cdf708ed61
Liam Owen <l1amow3n5@yahoo.com>
2019-02-05 12:36:25 -0700

    made my read methods for music and crime data

---
+++ CPPDataProject/Controller/FileController.cpp
@@ -0,0 +1,78 @@
+//
+//  FileController.cpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
+
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    if (dataFile.is_open())
+    {
+        
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '/r');
+            if(rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimeVector;
+}
+
+vector<Music> FileController :: MusicDataToVector(string filename)
+{
+    vector<Music> MusicVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    if (dataFile.is_open())
+    {
+        
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '/r');
+            if(rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    Music row(currentCSVLine);
+                    MusicVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return musicVector;
+}
---
+++ CPPDataProject/Controller/FileController.hpp
@@ -0,0 +1,30 @@
+//
+//  FileController.hpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    static vector<Music> musicDataToVecotr(string filename);
+    
+};
+
+#endif /* FileController_hpp */
