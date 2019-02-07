27c31f7600fee45b396e99c80b123126e4f67348
Liam Owen <l1amow3n5@yahoo.com>
2019-02-07 12:18:27 -0700

    made a new template class called List

---
+++ CPPDataProject/Model/Linear/List.hpp
@@ -0,0 +1,32 @@
+//
+//  List.hpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+
+#include "../Nodes/LinearNode.hpp"
+
+template <class Type>
+
+class List
+{
+protected:
+    int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+#endif /* List_h */
