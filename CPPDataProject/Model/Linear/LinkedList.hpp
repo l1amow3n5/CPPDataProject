2b73781eceee700468fc7e02e341b5b57284d93b
Liam Owen <l1amow3n5@yahoo.com>
2019-02-07 13:10:25 -0700

    made a new file for the LinkedList in the linear folder

---
+++ CPPDataProject/Model/Linear/LinkedList.hpp
@@ -0,0 +1,17 @@
+//
+//  LinkedList.hpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include "List.hpp"
+
+using namespace std;
+
+
+#endif /* LinkedList_h */
