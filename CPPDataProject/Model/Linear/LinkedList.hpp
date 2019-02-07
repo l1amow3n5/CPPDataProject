269aa35c21b2a5d8628d36124f47c8fcbb937c81
Liam Owen <l1amow3n5@yahoo.com>
2019-02-07 13:18:50 -0700

    made my constructor with the constructor deconstructor and methods

--- CPPDataProject/Model/Linear/LinkedList.hpp
+++ CPPDataProject/Model/Linear/LinkedList.hpp
@@ -13,5 +13,26 @@
 
 using namespace std;
 
-
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    //Constructors
+    LinkedList();
+    //Destructor
+    virtual ~LinkedList(); //Virtural so it can be overridden.
+    //Helper Methods
+    int getSize() const;
+    LinearNode<Type> * getFront;
+    LinearNode<Type> * getEnd;
+    
+    //Structor Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+};
 #endif /* LinkedList_h */
