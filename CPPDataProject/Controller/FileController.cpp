f7a3358a00bded3dae984672a85d5d14cb972028
Liam Owen <l1amow3n5@yahoo.com>
2019-02-13 13:25:57 -0700

    made the music and crime data to list method  in the FIleController.cpp

--- CPPDataProject/Controller/FileController.cpp
+++ CPPDataProject/Controller/FileController.cpp
@@ -76,3 +76,77 @@ vector<Music> FileController :: MusicDataToVector(string filename)
     
     return MusicVector;
 }
+
+LinkedList<CrimeData> FileController :: readDataToList(string fileName)
+{
+    LinkedList<CrimeData> crimes;
+    
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(fileName);
+    
+    // If the file exists at that path
+    if (dataFile.is_open())
+    {
+        
+        //keep reading until you are at the end of the file
+        while (!dataFile.eof())
+        {
+            //Grab each line from the CSV seperated by the carriage return character
+            getline(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if (rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                   {
+                       CrimeData row(currentCSVLine);
+                       crimes.add(row)
+                   }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimes;
+}
+
+LinkedList<Music> FileController :: musicDataToList(string fileName)
+{
+    LinkedList<Music> musicList;
+    
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(fileName);
+    
+    if(dataFile.is_open())
+    {
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '\r');
+            
+            if (rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    Music row(currentCSVLine);
+                    musicList.add(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return musicList;
+}
