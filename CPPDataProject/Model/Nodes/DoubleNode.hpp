4c1341782121d2b8d2e14f8e8baf894547aaa77d
Liam Owen <l1amow3n5@yahoo.com>
2019-02-21 12:02:13 -0700

    finished the doubleNode

--- CPPDataProject/Model/Nodes/DoubleNode.hpp
+++ CPPDataProject/Model/Nodes/DoubleNode.hpp
@@ -47,8 +47,32 @@ DoubleNode<Type> :: DoubleNode(Type data) : Node<Type>(data)
 template <class Type>
 DoubleNode<Type> :: DoubleNode(Type data, DoubleNode<Type> *previous, DoubleNode<Type>* next) : Node<Type>(data)
 {
-    this->previous = previous; 
+    this->previous = previous;
     this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setNext(DoubleNode<Type>* next)
+{
+    this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setPrevious(DoubleNode<Type>* previous)
+{
+    this->previous = previous;
+}
+
+template <class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getPrevious()
+{
+    return previous;
+}
+
+template <class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getNext()
+{
+    return next;
 }
 
 #endif /* DoubleNode_h */
