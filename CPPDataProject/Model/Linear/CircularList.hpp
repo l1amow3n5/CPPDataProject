4fbb63eb7c3f7ef5f9e8639d572a16d4ea0ce23f
Liam Owen <l1amow3n5@yahoo.com>
2019-02-21 12:22:12 -0700

    made the class section with the constructors data members and methods

--- CPPDataProject/Model/Linear/CircularList.hpp
+++ CPPDataProject/Model/Linear/CircularList.hpp
@@ -9,5 +9,31 @@
 #ifndef CircularList_h
 #define CircularList_h
 
+#include "../Nodes/DoubleNode.hpp"
+
+template <class Type>
+class CircularList = public LinkedList<Type>
+{
+private:
+    DoubleNode<Type> * front;
+    DoubleNode<Type> * end;
+    int size;
+    
+    DoubleNode<Type> * findNode(int index);
+    
+public:
+    CircularList();
+    ~CurcularList();
+    
+    //List methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type remove (int index);
+    Type setAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    int getSize() const;
+};
+
+
 
 #endif /* CircularList_h */
