ac2efba251f1cc0d520d2e91ae84c966a8614d6a
Liam Owen <l1amow3n5@yahoo.com>
2019-04-30 11:59:33 -0600

    making a new non linear file called AVLTree

---
+++ CPPDataProject/Model/NonLinear/AVLTree.hpp
@@ -0,0 +1,66 @@
+//
+//  AVLTree.hpp
+//  CPPDataProject
+//
+//  Created by Owen, Liam on 4/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_h
+#define AVLTree_h
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree (BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+template <class Type>
+AVLTree<Type> :: AVLTree() : BinarySearchTree<Type>()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item);
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    
+}
+#endif /* AVLTree_h */
